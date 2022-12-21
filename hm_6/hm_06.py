0ffba491c81791c4757537beb897530b55339c5c
lolitakhalilova <119189016+lolitakhalilova@users.noreply.github.com>
2022-12-21 16:52:54 +0100

    added 6th homework

---
+++ hm_6/hm_06.py
@@ -0,0 +1,37 @@
+phone_book = {
+    "Alina Kravchenko": "0664686501",
+    "Egor Miroshnichenko": "0995678456",
+    "Ruslan Volikov": "0987656342",
+    "Nastya Lehenka": "0995676456"
+
+}
+while True:
+    user_input = input("Enter a command here: ")
+    split_input = user_input.split()
+
+    if user_input == "stats":
+        contacts_amount = len(phone_book)
+        print(contacts_amount)
+
+    if user_input == "list":
+        all_names = phone_book.keys()
+        for name in all_names:
+            print(name)
+
+    if "show" in user_input:
+        name = split_input[1] + " " + split_input[2]
+        print(phone_book.get(name.title()))
+
+    if "add" in user_input:
+        new_name = split_input[1].title() + " " + split_input[2].title()
+        new_contact = [new_name, split_input[3]]
+        phone_book.update({new_contact[0]: new_contact[1]})
+
+    if "delete" in user_input:
+        key = split_input[1].title() + " " + split_input[2].title()
+        del phone_book[key]
+
+
+
+
+

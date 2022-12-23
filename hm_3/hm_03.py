9db1f1efd29c5ca6cafafa1d6073dd143e904771
lolitakhalilova <119189016+lolitakhalilova@users.noreply.github.com>
2022-12-23 14:01:25 +0100

    hm has been corrected

--- hm_3/hm_03.py
+++ hm_3/hm_03.py
@@ -1,25 +1,26 @@
-# task_1
+# Task 1
 string = "I love Python" * 42
 print(string)
-# task_2
+# Task 2
 age_in_month = 24 * 12 + 10
 print(age_in_month)
-# task_3
+# Task 3
 age_in_years = age_in_month // 12
 print(age_in_years)
-# task_4
+# Task 4
 name = "Lolita"
 my_age = f"My name is {name}. I'm {age_in_years} years old"
 print(my_age)
-# task_5
+# Task 5
 i = 1
 print(2 != i)  # True
 print(4 == i)  # False
 print(1 >= i)  # True
 print(5 < i)   # False
 print(8 > i)   # True
-# task_6gi
+# Task 6
 a = 2
 b = 5
 c = 6
-d = a ** (a + c)
+d = str(a ** ((a + b) + (c - b)))
+print(d)

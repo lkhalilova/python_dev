fc2d5c7f524049bd1decfee034c37a87d65de746
lolitakhalilova <119189016+lolitakhalilova@users.noreply.github.com>
2023-04-08 02:55:08 +0200

    infinite loops deleted, weather_url changed, variables names concretized

--- hm_30/hm_030.py
+++ hm_30/hm_030.py
@@ -2,28 +2,27 @@ import requests
 import random
 import json
 
-GOOGLE = "https://www.google.com/"
-FACEBOOK = "https://www.facebook.com/"
-TWITTER = "https://twitter.com/"
-AMAZON = "https://www.amazon.com/"
-APPLE = "https://www.apple.com/"
-ALL = [GOOGLE, FACEBOOK, TWITTER, AMAZON, APPLE]
-while True:
-    for item in ALL:
-        res = requests.get(random.choice(ALL))
-        print(f"url:{item}, status_code:{res.status_code}, html_length:{len(res.text)}")
+url = (["https://www.google.com/", "https://www.facebook.com/",
+        "https://twitter.com/", "https://www.amazon.com/", "https://www.apple.com/"])
 
-import requests
+for item in url:
+    res = requests.get(random.choice(url))
+    print(f"url:{item}, status_code:{res.status_code}, html_length:{len(res.text)}")
 
-while True:
-    WEATHER_API = "https://api.open-meteo.com/v1/forecast?"
-    CITY_API = "https://geocoding-api.open-meteo.com/v1/search"
-    name = input("Enter a name of a city here: ")
-    r_2 = requests.get(f"{CITY_API}?name={name.title()}&language=en&count=10&format=json")
-    data = r_2.json()
-    results = data.get("results")
-    dict = results[0]
-    r = requests.get(f"{WEATHER_API}latitude={dict['latitude']}&longitude={dict['longitude']}&hourly=temperature_2m")
-    data_2 = r.json()
-    print(data_2)
+
+WEATHER_API = "https://api.open-meteo.com/v1/forecast?"
+CITY_API = "https://geocoding-api.open-meteo.com/v1/search"
+name = input("Enter a name of a city here: ")
+try:
+    geo_response = requests.get(f"{CITY_API}?name={name.title()}&language=en&count=10&format=json")
+    geo_data = geo_response.json()
+    geo_results = geo_data.get("results")
+    city_geo_data = geo_results[0]
+except TypeError:
+    print("No such city")
+else:
+    r = requests.get(f"{WEATHER_API}latitude={city_geo_data['latitude']}&longitude={city_geo_data['longitude']}"
+                     f"&current_weather=true")
+    weather_data = r.json().get("current_weather")
+    print(weather_data)
 

e477f7ef0dafadd60ca7562fd68ef6233090cf9b
lolitakhalilova <119189016+lolitakhalilova@users.noreply.github.com>
2023-05-07 19:00:33 +0200

    flask hm added

---
+++ Flask_hm/run.py
@@ -0,0 +1,6 @@
+from Flask_homework import app
+
+if __name__ == "__main__":
+    app.run(host=app.config.get("HOST"),
+            port=app.config.get("PORT"),
+            debug=app.config.get("DEBUG"))

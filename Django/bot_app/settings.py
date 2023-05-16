b52d045df4ae242ca60950e37cfd21bff3ebea4c
lolitakhalilova <119189016+lolitakhalilova@users.noreply.github.com>
2023-05-16 05:22:08 +0200

    postgres db was added to settings.py

--- Django/bot_app/settings.py
+++ Django/bot_app/settings.py
@@ -92,9 +92,12 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ["POSTGRES_HOST"],
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
     }
 }
 
